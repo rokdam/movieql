e5d49587e6b165d25a9ae598b7dc5c15057f5d31
rokdam <travel_dob@naver.com>
2021-08-08 18:15:31 +0900

    스키마,resolver작성

---
+++ graphql/resolvers.js
@@ -0,0 +1,9 @@
+//쿼리를 해결하는 파일
+const resolvers = {
+    Query : {
+        name: () => "nico"
+
+    }
+}
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { GraphQLServer } = require("graphql-yoga");
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers"
 
 const server = new GraphQLServer({
     //환경설정을 여기 넣어주면 됨
+    typeDefs : "./graphql/schema.graphql",
+    resolvers
 })
 
 server.start(() => console.log("GraphQL Server Running "))
