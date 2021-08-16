a7bb6a957a61ace820889c5c0876ef6dad9af2bd
rokdam <travel_dob@naver.com>
2021-08-16 20:22:57 +0900

    영화추가 및 삭제기능 추가

--- graphql/db.js
+++ graphql/db.js
@@ -19,8 +19,59 @@ export const people = [
     }
 ];
 
+let movies = [
+    {
+        id: 0,
+        name : "Start Wars - The new one",
+        score : 1
+    },
+    {
+        id: 1,
+        name : "Avengers - The new one",
+        score : 8
+    },
+    {
+        id: 2,
+        name : "The Godfather I",
+        score : 99
+    },
+    {
+        id: 3,
+        name : "Logan",
+        score : 2
+    }
+    
+]
+
 export const getById = id => {
     //filter는 모든 대상을 거쳐 해당 조건에 맞는 걸 리턴하는 것 
     const filteredPeople = people.filter(person => id === person.id);
     return filteredPeople[0];
+}
+
+export const getMovies = () => movies;
+export const getMovieById = id => {
+    const filteredMovie = movies.filter(movie => movie.id === id);
+    return filteredMovie[0];
+}
+
+export const deleteMovie = (id) => {
+    //선택한 id빼고 남은것만 저장(선택한 id값을 지움)
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    }else{
+        return false;
+    }
+}
+
+export const addMovie = (name, score) =>{
+    const newMovie = {
+        id: `${movies.length + 1}`, // movies배열 길이에 더하기 1
+        name,
+        score,
+    }
+    movies.push(newMovie);
+    return newMovie;
 }
